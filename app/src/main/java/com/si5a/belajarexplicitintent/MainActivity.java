944f4c7f45557330d2e4ce5309aea677239f0426
valentinolie24 <valenlie2409@gmail.com>
2022-10-10 14:51:28 +0700

    Update 1

--- app/src/main/java/com/si5a/belajarexplicitintent/MainActivity.java
+++ app/src/main/java/com/si5a/belajarexplicitintent/MainActivity.java
@@ -2,13 +2,42 @@ package com.si5a.belajarexplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Layout A");
+
+        btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+
+                if(nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi");
+                }
+                else{
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("xNama", nama);
+                    startActivity(pindah);
+                }
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/si5a/belajarexplicitintent/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.si5a.belajarexplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView tvNama;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setTitle("Layout B");
+
+        tvNama = findViewById(R.id.tv_nama);
+
+        Intent terima = getIntent();
+        String yNama = terima.getStringExtra("xNama");
+        tvNama.setText(yNama);
+    }
+}
